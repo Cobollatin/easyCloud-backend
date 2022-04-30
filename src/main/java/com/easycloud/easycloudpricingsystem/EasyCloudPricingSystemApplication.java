171a551df37efd54e65dbd758cd0a7b6df6905a9
Cobollatin <Cobollatin@hotmail.com>
2022-04-30 09:30:13 -0500

    setup

---
+++ src/main/java/com/easycloud/easycloudpricingsystem/EasyCloudPricingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.easycloud.easycloudpricingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCloudPricingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCloudPricingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easycloud/easycloudpricingsystem/EasyCloudPricingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easycloud.easycloudpricingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCloudPricingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
